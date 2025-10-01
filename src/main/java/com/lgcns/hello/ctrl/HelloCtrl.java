75ef0924685954ddcbf54783cdfc8dda63446423
leesurim <leesurim@leesurim.>
2025-10-01 13:10:43 +0900

    WSL - JAVA 17 설치 & 간단한 스프링 프로젝트 만들기

---
+++ src/main/java/com/lgcns/hello/ctrl/HelloCtrl.java
@@ -0,0 +1,33 @@
+package com.lgcns.hello.ctrl;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloCtrl {
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return a + b ;
+    }
+
+    @GetMapping("/sub")
+    public int sub(@RequestParam int a, @RequestParam int b) {
+        return a - b ;
+    }
+
+    @GetMapping("/mul")
+    public int mul(@RequestParam int a, @RequestParam int b) {
+        return a * b ;
+    }
+
+    @GetMapping("/div")
+    public String div(@RequestParam int a, @RequestParam int b) {
+        if ( b == 0 ) {
+            return "Error: Division by Zero!";
+        }
+        return String.valueOf(a / b) ;
+    }
+    
+}
