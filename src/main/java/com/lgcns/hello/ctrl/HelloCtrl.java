bd6494b37dbc6b6325290a1d3b30c930494025ce
surimleeee <90364499+surimleeee@users.noreply.github.com>
2025-10-07 14:40:46 +0900

    Update HelloCtrl.java

--- src/main/java/com/lgcns/hello/ctrl/HelloCtrl.java
+++ src/main/java/com/lgcns/hello/ctrl/HelloCtrl.java
@@ -1,11 +1,14 @@
 package com.lgcns.hello.ctrl;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
+
 @RestController
+@@RequestMapping("/hello")
 public class HelloCtrl {
     @GetMapping("/add")
     public int add(@RequestParam int a, @RequestParam int b) {
@@ -31,3 +34,4 @@ public class HelloCtrl {
     }
     
 }
+
